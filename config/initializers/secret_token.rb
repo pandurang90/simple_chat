db3ef21f7e642286ebdc7d24ee209f25ec359d7c
pandurang <pandurang.plw@gmail.com>
2013-07-18 17:24:27 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleChat::Application.config.secret_token = '783e5d9ff3358ac0096bf694d1f496703e59c67924dce6d4149bffb7b0d1b8e83d01887caf4bacce5107626d28d3803a4c9dc7404ee1d41ee2d130f16bdd139f'
